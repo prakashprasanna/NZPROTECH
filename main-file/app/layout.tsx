23d56ffc485fdaa58769b62fc1c2fe6d532a692c
Prakash Prasanna <32215290+prakashprasanna@users.noreply.github.com>
2024-08-15 22:03:00 +1200

    remove google analytics form doc index and added in layout

--- main-file/app/layout.tsx
+++ main-file/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/public/styles/style.scss";
 import InitAnimations from "@/components/layout/InitAnimations";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "AMPRO | Software Development & IT Solutions",
@@ -29,6 +30,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+            {/* <!-- Google tag (gtag.js) --> */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-FQ0H1RLLHD"></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-FQ0H1RLLHD'), {
+          page_path: window.location.pathname,
+        };
+        `,
+        }} />
+      </head>
       <body>
         {children}
         <InitAnimations />
